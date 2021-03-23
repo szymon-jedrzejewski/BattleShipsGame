19cede991a75303e3db642716df7f00e3286efa9
Szymon Jedrzejewski <szymonj219@gmail.com>
2021-03-23 22:43:06 +0100

    Initial commit

---
+++ src/main/java/Coordinates.java
@@ -0,0 +1,34 @@
+public class Coordinates {
+
+    private String x;
+    private String y;
+
+    public Coordinates(String x, String y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinates{" +
+                "x='" + x + '\'' +
+                ", y='" + y + '\'' +
+                '}';
+    }
+}
