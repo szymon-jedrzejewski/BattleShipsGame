56e33bc3b3307a6cdc1f7e46251c045513f655df
Szymon Jedrzejewski <szymonj219@gmail.com>
2021-04-05 18:43:42 +0200

    Remove unnecessary classes

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
 import java.util.List;
 
-public interface Player {
-    Coordinate enterCoordinate();
-    List<Ship> getShips();
-    void createShip(Ship ship);
+public class Player {
+    List<Ship> ships = new ArrayList<>();
+
+    public Coordinate enterCoordinate(String x, String y) {
+        return new Coordinate(x, y);
+    }
+
+    public List<Ship> getShips() {
+        return ships;
+    }
+
+    public void addShip(Ship ship) {
+        ships.add(ship);
+    }
 }
