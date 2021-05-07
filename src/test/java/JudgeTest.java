ef9b05c4d6d8611172418a0da0c5cc52ac6f79f7
Szymon Jedrzejewski <szymonj219@gmail.com>
2021-05-07 23:42:27 +0200

    Write new test

--- src/test/java/JudgeTest.java
+++ src/test/java/JudgeTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 
 public class JudgeTest {
     private Ship ship;
-    private Coordinate coordinate;
 
     @Before
     public void setUp() {
@@ -14,13 +13,19 @@ public class JudgeTest {
         ship.addCoordinate(new Coordinate("B", "1"));
         ship.addCoordinate(new Coordinate("C", "1"));
         ship.addCoordinate(new Coordinate("D", "1"));
-
-        coordinate = new Coordinate("A", "1");
     }
 
     @Test
     public void shouldReturnTrueIfShipGotShot() {
         Judge judge = new Judge();
+        Coordinate coordinate = new Coordinate("A", "1");
         assertTrue(judge.wasShipHit(ship, coordinate));
+    }
+
+    @Test
+    public void shouldReturnFalseIfShipDidNotGetShot() {
+        Judge judge = new Judge();
+        Coordinate coordinate = new Coordinate("A", "2");
+        assertFalse(judge.wasShipHit(ship, coordinate));
     }
 }
