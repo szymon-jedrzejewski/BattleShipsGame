0724de737c4481dd41c760b870088cbb949236fc
Szymon Jedrzejewski <szymonj219@gmail.com>
2021-07-15 22:05:28 +0200

    Remove methods validating coords from 'Ship'

--- src/main/java/Ship.java
+++ src/main/java/Ship.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Ship {
 
@@ -40,51 +38,9 @@ public class Ship {
 
     private boolean areCoordsCorrect(List<Coordinate> coords) {
         if (type.getShipSize() > 1) {
-            if (!isTheSameLetter(coords) && !isTheSameNumber(coords)) return false;
-            if (isTheSameLetter(coords)) return areCorrectNumbersInCoords(coords);
-            if (isTheSameNumber(coords)) return areCorrectLettersInCoords(coords);
-        }
-        return true;
-    }
-
-    private boolean isTheSameLetter(List<Coordinate> coords) {
-        Set<String> letters = new HashSet<>();
-        for (Coordinate coordinate : coords) {
-            letters.add(coordinate.getX());
-        }
-        return letters.size() == 1;
-    }
-
-    private boolean isTheSameNumber(List<Coordinate> coords) {
-        Set<String> numbers = new HashSet<>();
-        for (Coordinate coordinate : coords) {
-            numbers.add(coordinate.getY());
-        }
-        return numbers.size() == 1;
-    }
-
-    private boolean areCorrectNumbersInCoords(List<Coordinate> coords) {
-        int previousNumberInCoordinate = Integer.parseInt(coords.get(0).getY());
-
-        for (int i = 1; i < coords.size(); i++) {
-            if (Integer.parseInt(coords.get(i).getY()) != previousNumberInCoordinate + 1) {
-                return false;
-            }
-            previousNumberInCoordinate = Integer.parseInt(coords.get(i).getY());
-        }
-        return true;
-    }
-
-
-    private boolean areCorrectLettersInCoords(List<Coordinate> coords) {
-        final String LETTERS = "ABCDEFGHIJ";
-        int indexOfFirstLetter = LETTERS.indexOf(coords.get(0).getX());
-
-        for (int i = 1; i < coords.size(); i++) {
-            if (indexOfFirstLetter + 1 != LETTERS.indexOf(coords.get(i).getX())) {
-                return false;
-            }
-            indexOfFirstLetter = LETTERS.indexOf(coords.get(i).getX());
+            if (!CoordinateValidator.isTheSameLetter(coords) && !CoordinateValidator.isTheSameNumber(coords)) return false;
+            if (CoordinateValidator.isTheSameLetter(coords)) return CoordinateValidator.areCorrectNumbersInCoords(coords);
+            if (CoordinateValidator.isTheSameNumber(coords)) return CoordinateValidator.areCorrectLettersInCoords(coords);
         }
         return true;
     }
