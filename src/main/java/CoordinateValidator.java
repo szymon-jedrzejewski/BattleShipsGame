1f429f008fe521df4da97c18ca218377d5c8e730
Szymon Jedrzejewski <szymonj219@gmail.com>
2021-07-15 22:04:44 +0200

    Move methods validating coords to separate class

---
+++ src/main/java/CoordinateValidator.java
@@ -0,0 +1,47 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CoordinateValidator {
+    public static boolean isTheSameLetter(List<Coordinate> coords) {
+        Set<String> letters = new HashSet<>();
+        for (Coordinate coordinate : coords) {
+            letters.add(coordinate.getX());
+        }
+        return letters.size() == 1;
+    }
+
+    public static boolean isTheSameNumber(List<Coordinate> coords) {
+        Set<String> numbers = new HashSet<>();
+        for (Coordinate coordinate : coords) {
+            numbers.add(coordinate.getY());
+        }
+        return numbers.size() == 1;
+    }
+
+    public static boolean areCorrectNumbersInCoords(List<Coordinate> coords) {
+        int previousNumberInCoordinate = Integer.parseInt(coords.get(0).getY());
+
+        for (int i = 1; i < coords.size(); i++) {
+            if (Integer.parseInt(coords.get(i).getY()) != previousNumberInCoordinate + 1) {
+                return false;
+            }
+            previousNumberInCoordinate = Integer.parseInt(coords.get(i).getY());
+        }
+        return true;
+    }
+
+
+    public static boolean areCorrectLettersInCoords(List<Coordinate> coords) {
+        final String LETTERS = "ABCDEFGHIJ";
+        int indexOfFirstLetter = LETTERS.indexOf(coords.get(0).getX());
+
+        for (int i = 1; i < coords.size(); i++) {
+            if (indexOfFirstLetter + 1 != LETTERS.indexOf(coords.get(i).getX())) {
+                return false;
+            }
+            indexOfFirstLetter = LETTERS.indexOf(coords.get(i).getX());
+        }
+        return true;
+    }
+}
---
+++ src/test/java/CoordinateValidatorTest.java
@@ -0,0 +1,91 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class CoordinateValidatorTest {
+
+    @Test
+    public void shouldPassIfIsTheSameLetter() {
+        List<Coordinate> coords = new ArrayList<>();
+        coords.add(new Coordinate("A", "1"));
+        coords.add(new Coordinate("A", "2"));
+        coords.add(new Coordinate("A", "3"));
+        coords.add(new Coordinate("A", "4"));
+        assertTrue(CoordinateValidator.isTheSameLetter(coords));
+    }
+
+    @Test
+    public void shouldNotPassIfIsNotTheSameLetter() {
+        List<Coordinate> coords = new ArrayList<>();
+        coords.add(new Coordinate("A", "1"));
+        coords.add(new Coordinate("B", "2"));
+        coords.add(new Coordinate("A", "3"));
+        coords.add(new Coordinate("A", "4"));
+        assertFalse(CoordinateValidator.isTheSameLetter(coords));
+    }
+
+    @Test
+    public void shouldPassIfIsTheSameNumber() {
+        List<Coordinate> coords = new ArrayList<>();
+        coords.add(new Coordinate("A", "1"));
+        coords.add(new Coordinate("A", "1"));
+        coords.add(new Coordinate("A", "1"));
+        coords.add(new Coordinate("A", "1"));
+        assertTrue(CoordinateValidator.isTheSameLetter(coords));
+    }
+
+    @Test
+    public void shouldNotPassIfIsNotTheSameNumber() {
+        List<Coordinate> coords = new ArrayList<>();
+        coords.add(new Coordinate("A", "1"));
+        coords.add(new Coordinate("B", "2"));
+        coords.add(new Coordinate("A", "3"));
+        coords.add(new Coordinate("A", "4"));
+        assertFalse(CoordinateValidator.isTheSameLetter(coords));
+    }
+
+    @Test
+    public void shouldPassIfThereAreCorrectLettersInCoords() {
+        List<Coordinate> coords = new ArrayList<>();
+        coords.add(new Coordinate("A", "1"));
+        coords.add(new Coordinate("B", "1"));
+        coords.add(new Coordinate("C", "1"));
+        coords.add(new Coordinate("D", "1"));
+        assertTrue(CoordinateValidator.areCorrectLettersInCoords(coords));
+    }
+
+    @Test
+    public void shouldNotPassIfThereAreNotCorrectLettersInCoords() {
+        List<Coordinate> coords = new ArrayList<>();
+        coords.add(new Coordinate("A", "1"));
+        coords.add(new Coordinate("B", "1"));
+        coords.add(new Coordinate("D", "1"));
+        coords.add(new Coordinate("E", "1"));
+        assertFalse(CoordinateValidator.areCorrectLettersInCoords(coords));
+    }
+
+    @Test
+    public void shouldPassIfThereAreCorrectNumbersInCoords() {
+        List<Coordinate> coords = new ArrayList<>();
+        coords.add(new Coordinate("B", "1"));
+        coords.add(new Coordinate("B", "2"));
+        coords.add(new Coordinate("B", "3"));
+        coords.add(new Coordinate("B", "4"));
+        assertTrue(CoordinateValidator.areCorrectNumbersInCoords(coords));
+    }
+
+    @Test
+    public void shouldNotPassIfThereAreNotCorrectNumbersInCoords() {
+        List<Coordinate> coords = new ArrayList<>();
+        coords.add(new Coordinate("B", "1"));
+        coords.add(new Coordinate("B", "2"));
+        coords.add(new Coordinate("B", "5"));
+        coords.add(new Coordinate("B", "4"));
+        assertFalse(CoordinateValidator.areCorrectNumbersInCoords(coords));
+    }
+
+
+}
